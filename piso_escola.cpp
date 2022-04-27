57dbe8a76cd3703e904fa9ee450af02fc0eba0ec
Francisco Jean <jeansousa2001@gmail.com>
2022-04-27 19:50:19 -0300

    Exercicios - 27/04/222

---
+++ piso_escola.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int l , c, t1, t2;
+    cin >> l;
+    cin >> c;
+
+    t1 = l * c + ((l-1) * (c - 1));
+    t2 = (l-1) * 2 + (c -1) * 2;
+    
+    cout << t1 << endl;
+    cout << t2 << endl;
+
+    return 0;
+}
